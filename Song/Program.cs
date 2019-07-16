d72ee79412458fec52ec360ea2c0b0bae25b1353
wwstudent <wwstudent@MS-9D04CH2>
2019-07-16 09:28:11 -0700

    This one should full update add ons

--- Song/Program.cs
+++ Song/Program.cs
@@ -4,15 +4,37 @@ namespace Song
 {
     class Program
     {
+        static void SingASong()
+        {
+            SingASong("Baby Shark");
+
+        }
+
         static void SingASong(string title)
         {
-            Console.WriteLine($"Let's sing {title}");
+            SingASong(title, "C");
+            
+        }
+
+        static void SingASong(string title, string key)
+        {
+            Console.WriteLine($"  Let's sing {title} in the key of {key}");
+            SingASong(title, key, 4);
+        }
+
+        static void SingASong(string title, string key, int nrTimes)
+        {
+            for (int i = 0; i < nrTimes; i++)
+            {
+                Console.WriteLine($"  sing: {title} (in {key})");
+            }
         }
 
         static void Main(string[] args)
         {
             Console.WriteLine("\n\tSong.Program.Main()\n");
-            SingASong("Baby Shark!");
+            SingASong();
+            SingASong("Baby Shark!", "F#");
         }
     }
 }
